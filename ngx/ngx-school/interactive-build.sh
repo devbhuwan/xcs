49b657f88794e1cd98d4e0a1fd4aff2a4892d5f1
Bhuwan Upadhyay <bhuwan.upadhyay49@gmail.com>
2018-01-04 08:49:15 +0400

    ngx

---
+++ ngx/ngx-school/interactive-build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# Bash Menu Script Example
+
+PS3='Please enter your choice: '
+options=("Normal Build" "Normal build and publish" "Run App" "Run Test" "Install Developer Dependencies" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Normal Build")
+            echo "you chose 'Normal Build'"
+            npm run build && cd dist/lib && yarn pack
+            ;;
+        "Normal build and publish")
+            echo "you chose 'Normal build and publish'"
+            printf 'New Version [] : '
+            read newVersion
+            sed -i -E "s/\"version\":.*[^,]/\"version\":\"${newVersion}\"/g" package.json
+            npm run build && cd dist/lib && yarn publish
+            ;;
+        "Run App")
+            echo "you chose 'Run App'"
+            npm run start
+            ;;
+        "Run Test")
+            echo "you chose 'Run Test'"
+            printf 'Matching File Name : '
+            read fileName
+            npm run test ${fileName}
+            ;;
+        "Install Developer Dependencies")
+            echo "you chose 'Run Test'"
+            yarn add --dev jest jest-preset-angular @types/jest mocha karma-mocha-reporter karma-phantomjs-launcher electron karma-electron-launcher  ng-packagr cpr ts-mockito
+             yarn add -dev
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo invalid option;;
+    esac
+done
