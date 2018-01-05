ecbeea244db7f5e5ece9d2200c2c44ee7fd8b8fe
Bhuwan Upadhyay <bhuwan.upadhyay49@gmail.com>
2018-01-05 23:14:59 +0400

    Baby.M1

--- security/uaa-client-keycloak-starter/src/test/java/uaa/client/keycloak/AbstractUaaKeycloakIntegrationTests.java
+++ security/uaa-client-keycloak-starter/src/test/java/uaa/client/keycloak/AbstractUaaKeycloakIntegrationTests.java
@@ -1,6 +1,5 @@
 package uaa.client.keycloak;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.context.embedded.LocalServerPort;
@@ -16,10 +15,6 @@ public class AbstractUaaKeycloakIntegrationTests {
 
     @Test
     public void contextLoads() {
-    }
-
-    @Before
-    public void setUp() {
     }
 
 
