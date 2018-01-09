70a32050ba3d9f9d36818690b2751aea7a325a8d
Bhuwan Upadhyay <bhuwan.upadhyay49@gmail.com>
2018-01-09 23:17:08 +0400

    Baby.M1

--- security/kinght.sh
+++ security/kinght.sh
@@ -1,25 +1,18 @@
 #!/bin/bash -e
 
-function run-integration-tests() {
-   exit
-}
-
 function buildWithDockerImage() {
     echo "Clean and Build Project"
     ./gradlew clean build copyDocker docker -x test
-    exit 0
 }
 
 function publishToCloudfoundry() {
-    echo "Publish To CF"
+    echo "Publish To Cloudfoundry"
     cf push
-    exit 0
 }
 
 function lunchDockerContainers() {
-    echo "Publish To CF"
+    echo "Launch Local Docker Running Containers"
     docker-compose up
-    exit
 }
 
 buildWithDockerImage && publishToCloudfoundry
