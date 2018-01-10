1fcc50a1561eb21001d793ebef12c70ca2ac80e6
Bhuwan Upadhyay <bhuwan.upadhyay49@gmail.com>
2018-01-10 22:44:54 +0400

    Baby.M1

--- cloudservices/config-provider/src/main/java/cloud/services/config/provider/CloudConfigProviderApplication.java
+++ cloudservices/config-provider/src/main/java/cloud/services/config/provider/CloudConfigProviderApplication.java
@@ -2,16 +2,16 @@ package cloud.services.config.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.ConfigServerApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableDiscoveryClient
 @EnableConfigServer
-public class CloudServiceConfigProviderApplication {
+public class CloudConfigProviderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
